d47a253eb67b61559c32938088d1888a978f55f9
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-05-12 16:40:25 +0500

    done box changes of admin dashboard

--- app/Http/Controllers/admin/AdminDashboardController.php
+++ app/Http/Controllers/admin/AdminDashboardController.php
@@ -7,11 +7,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Models\UserRole;
+use App\Models\Media;
 
 class AdminDashboardController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $total_users =  User::where('role_id', 2)->count();
+        $free_users =  User::where(['role_id' => 2, 'plan_id' => 1])->count();
+        $subscription_users =  User::where('role_id', 2)->whereNotIn('plan_id', [1])->count();
+        $total_media =  Media::count();
+        return view('admin.index', compact(
+            'total_users',
+            'free_users',
+            'subscription_users',
+            'total_media'
+        ));
     }
     public function allUsers(){
         $data   =   [
