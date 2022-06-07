9a60b4d92ffea67ca963184b50afbafad84656de
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-06-07 20:09:27 +0500

    add button for in email template

--- resources/views/emails/recipientMail.blade.php
+++ resources/views/emails/recipientMail.blade.php
@@ -8,10 +8,19 @@
 <body>
     <p>Dear {{ Auth::user()->name }} {{ Auth::user()->last_name }}</p>
 
-    <p>You have selected {{ $first_name }} {{ $last_name }} as your {{ $contact_status }} contact to ensure your well being.  Please confirm or deny by clicking here.</p>
+    <p>You have selected {{ $first_name }} {{ $last_name }} as your {{ $contact_status }} contact to ensure your well being. Please confirm or deny by clicking here.</p>
 
     <p>Thank you</p>
     <p>bETERNAL Team</p>
+
+    <div class="row pt-3 pb-5 media-icons">
+        <div class="col-lg-2">
+            <a href="http://167.99.0.236/"><button class="filter-btn btn w-100 text-center py-2">DENY</button></a>
+        </div>
+        <div class="col-lg-2">
+            <a href="http://167.99.0.236/"><button class="filter-btn btn w-100 text-center py-2">CONFIRM</button></a>
+        </div>
+    </div>
 </body>
 
 </html>
--- resources/views/emails/toRecipientMail.blade.php
+++ resources/views/emails/toRecipientMail.blade.php
@@ -12,6 +12,15 @@
 
     <p>Thank you</p>
     <p>{{ Auth::user()->name }} {{ Auth::user()->last_name }}</p>
+
+    <div class="row pt-3 pb-5 media-icons">
+        <div class="col-lg-2">
+            <a href="http://167.99.0.236/"><button class="filter-btn btn w-100 text-center py-2">DENY</button></a>
+        </div>
+        <div class="col-lg-2">
+            <a href="http://167.99.0.236/"><button class="filter-btn btn w-100 text-center py-2">CONFIRM</button></a>
+        </div>
+    </div>
 </body>
 
 </html>
