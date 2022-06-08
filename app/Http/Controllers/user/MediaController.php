3e90fd093fcb774900763cf9dc691ce4a1253792
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-06-08 19:51:14 +0500

    show progress msg

--- app/Http/Controllers/user/MediaController.php
+++ app/Http/Controllers/user/MediaController.php
@@ -87,6 +87,7 @@ class MediaController extends Controller
 
     public function uploadMedia(Request $request)
     {
+        dd('In Progress');
         if ($request->media_type == 'video') {
             $this->validate($request, [
                 'file_name' => 'required|file|mimetypes:video/mp4,video/avi,video/mpeg,video/quicktime',
@@ -110,7 +111,6 @@ class MediaController extends Controller
         }
 
         if ($request->upload_type == "media") {
-            dd('In Progress');
             $media = new Media();
             $media->title = $request->title;
             $media->description = $request->description;
@@ -160,7 +160,6 @@ class MediaController extends Controller
             }
         }
         if ($request->upload_type == "legacy") {
-            dd('In Progress');
             $legacy = new Legacy();
             $legacy->title = $request->title;
             $legacy->description = $request->description;
