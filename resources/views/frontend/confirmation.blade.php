938069d402f0a4475f88b8162e422f9f88d9b346
OsamaDesignerMasology <osama.designer.masology@gmail.com>
2022-06-15 17:38:34 +0500

    Confirm modal

--- resources/views/frontend/confirmation.blade.php
+++ resources/views/frontend/confirmation.blade.php
@@ -20,19 +20,19 @@
     </div>
 </div>
 @elseif(isset($token))
-<div class="modal-dialog">
+<div class="modal-dialog reject-modal modal-sm">
     <div class="modal-content">
         <div class="modal-body">
             <div class="row pt-3 pb-5 media-icons mt-4">
                 <div class="col-lg-3"></div>
                 <div class="col-lg-3">
                     <a href="{{ route('index') }}">
-                        <button class="filter-btn btn w-100 text-center py-2">REJECT</button>
+                        <button class="filter-btn btn w-100 text-center py-2 mt-2">REJECT</button>
                     </a>
                 </div>
                 <div class="col-lg-3">
                     <a href="{{ url('confirmation-success/'.$token) }}">
-                        <button class="filter-btn btn w-100 text-center py-2">CONFIRM</button>
+                        <button class="filter-btn btn w-100 text-center py-2 mt-2">CONFIRM</button>
                     </a>
                 </div>
                 <div class="col-lg-3"></div>
