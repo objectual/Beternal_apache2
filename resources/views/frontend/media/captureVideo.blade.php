74fb04c9587362855ab6d0b481ee1c9e3f7be60d
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-08-04 15:38:14 +0500

    changes for android

--- resources/views/frontend/media/captureVideo.blade.php
+++ resources/views/frontend/media/captureVideo.blade.php
@@ -29,7 +29,7 @@
                                 <div class="error">{{ $errors->first('file_name') }}</div>
                                 @endif
                                 <label class="record-images" style="color: #F7DB02;" for="file">&nbsp;&nbsp;Device Gallery</label>
-                                <input type="file" accept="video/*" name="file_name" id="file" style="display: none;" onchange="loadFile(event)">
+                                <input type="file" accept="video/*" name="file_name" id="file" style="display: none;" onchange="loadFile(event)" capture />
                             </a>
                         </div>
                         <div class="col-md-4 text-center">
