f4b2421da9d46b9ee89489b75320ae195234207a
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-08-04 18:34:22 +0500

    new changes for audio

--- resources/views/frontend/media/captureAudio.blade.php
+++ resources/views/frontend/media/captureAudio.blade.php
@@ -12,6 +12,20 @@
                 <div class="col-lg-3"></div>
                 <div class="col-lg-6 mt-4">
                     <div class="d-flex justify-content-between mt-4">
+                        @if((new \Jenssegers\Agent\Agent())->isMobile())
+                        <div class="col-md-4 text-center">
+                            <a>
+                                <div class="pb-3 media-icon-height">
+                                    <label class="record-images" style="color: #F7DB02;" for="file"><img src="{{ asset('/public/assets/images/audio.png') }}" class="record-img"></label>
+                                </div>
+                                @if($errors->has('file_name'))
+                                <div class="error">{{ $errors->first('file_name') }}</div>
+                                @endif
+                                <label class="record-images" style="color: #F7DB02;" for="file">&nbsp;&nbsp;Record Audio</label>
+                                <input type="file" accept="audio/*" name="file_name" id="file" style="display: none;" onchange="loadFile(event)" capture="user" />
+                            </a>
+                        </div>
+                        @else
                         <div class="col-md-4 text-center">
                             <a href="#" data-bs-toggle="modal" data-bs-target="#captureImage">
                                 <div class="pb-3 media-icon-height">
@@ -32,6 +46,7 @@
                                 <input type="file" accept="audio/*" name="file_name" id="file" style="display: none;" onchange="loadFile(event)">
                             </a>
                         </div>
+                        @endif
                         <div class="col-md-4 text-center">
                             <a href="{{ route('user.media.my-media') }}">
                                 <div class="pb-3 media-icon-height">
@@ -58,7 +73,7 @@
                             document.getElementById("attachment").style.display = "block";
                             var audio_url = URL.createObjectURL(event.target.files[0]);
                             $('#ban_audio').append(
-                                '<source src="'+ audio_url +'" type="audio/mp3" />'
+                                '<source src="'+ audio_url +'" />'
                             );
                         };
                     </script>
