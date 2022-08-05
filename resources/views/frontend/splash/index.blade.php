75a54573a35cc8f89223c944aecf94ad6633548d
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-08-05 15:32:12 +0500

    iframe testing on ios

--- resources/views/frontend/splash/index.blade.php
+++ resources/views/frontend/splash/index.blade.php
@@ -39,7 +39,11 @@
                                     <source src="{{ asset('/public/assets/images/solution.mp4') }}" type="video/mp4" />
                                     Your browser does not support the video tag.
                                 </video>
-                                <iframe width="420" height="315" src="{{ asset('/public/assets/images/solution.mp4') }}"></iframe>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 p-0-m">
+                                <iframe class="landing_video" width="420" height="315" src="{{ asset('/public/assets/images/solution.mp4') }}"></iframe>
                             </div>
                         </div>
                         <p class="sp-head-2">
