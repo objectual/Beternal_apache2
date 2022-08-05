678314647096e322ea0e9dc11a7b5be80d6ac1ec
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-08-05 15:50:43 +0500

    testing video display for ios

--- resources/views/frontend/splash/index.blade.php
+++ resources/views/frontend/splash/index.blade.php
@@ -36,7 +36,7 @@
                         <div class="row">
                             <div class="col-lg-12 p-0-m">
                                 <video class="landing_video" controls>
-                                    <source src="{{ asset('/public/assets/images/solution.mp4') }}" type="video/mp4" />
+                                    <source src="{{ asset('/public/assets/images/solution.mp4#t=0.001') }}" type="video/mp4" />
                                     Your browser does not support the video tag.
                                 </video>
                             </div>
