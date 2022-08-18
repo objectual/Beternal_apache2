e2ab70cca06bae2efc285fc74f7834e210381fdf
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-08-18 16:43:27 +0500

    done new changes for push notification

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,33 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
+/*
+Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+*/
+firebase.initializeApp({
+    apiKey: "AIzaSyDmlagHFn1yw5KcbXHuIfuuWsw2EcXTmwE",
+    authDomain: "notification-test-a3f05.firebaseapp.com",
+    projectId: "notification-test-a3f05",
+    storageBucket: "notification-test-a3f05.appspot.com",
+    messagingSenderId: "299026161686",
+    appId: "1:299026161686:web:53bf04a964fb438be01537",
+    measurementId: "G-RT6CHXQZTF"
+});
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDmlagHFn1yw5KcbXHuIfuuWsw2EcXTmwE",
-  authDomain: "notification-test-a3f05.firebaseapp.com",
-  projectId: "notification-test-a3f05",
-  storageBucket: "notification-test-a3f05.appspot.com",
-  messagingSenderId: "299026161686",
-  appId: "1:299026161686:web:53bf04a964fb438be01537",
-  measurementId: "G-RT6CHXQZTF"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log("Message received.", payload);
+    const title = "Hello world is awesome";
+    const options = {
+        body: "Your notificaiton message .",
+        icon: "/firebase-logo.png",
+    };
+    return self.registration.showNotification(
+        title,
+        options,
+    );
+});
