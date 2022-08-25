8406374867254a5158491a970f2b988cb691dcef
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-08-25 16:02:27 +0500

    logo changes

--- resources/views/frontend/layouts/layout.blade.php
+++ resources/views/frontend/layouts/layout.blade.php
@@ -10,7 +10,7 @@
 
         <!-- PWA  -->
         <meta name="theme-color" content="#6777ef"/>
-        <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+        <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
         <link rel="manifest" href="{{ asset('/manifest.json') }}">
     </head>
     <body>   
