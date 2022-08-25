f0bf0b234be36eb8d94bbd9536b394c0446e0efe
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-08-25 15:37:17 +0500

    pwa testing

--- resources/views/frontend/layouts/layout.blade.php
+++ resources/views/frontend/layouts/layout.blade.php
@@ -7,6 +7,11 @@
         <!-- Bootstrap CSS -->
         <title>@yield("title","bETERNAL")</title>
         @include("frontend.layouts.styles")
+
+        <!-- PWA  -->
+        <meta name="theme-color" content="#6777ef"/>
+        <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+        <link rel="manifest" href="{{ asset('/manifest.json') }}">
     </head>
     <body>   
         <div class="container-fluid h-100">
@@ -23,5 +28,14 @@
         </div>
           <!-- Links -->
             @include("frontend.layouts.scripts")
+
+        <script src="{{ asset('/sw.js') }}"></script>
+        <script>
+            if (!navigator.serviceWorker.controller) {
+                navigator.serviceWorker.register("/sw.js").then(function (reg) {
+                        console.log("Service worker has been registered for scope: " + reg.scope);
+                });
+            }
+        </script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,10 @@ use App\Http\Controllers\SubscriptionController;
 
 Route::get('/', [HomeController::class, 'index'])->name('index');
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 // start routes for ajax request
 Route::get('/provinces/{id}', [StateProvinceController::class, 'getStateProvinces']);
 Route::get('/cities/{id}', [CityController::class, 'getCities']);
