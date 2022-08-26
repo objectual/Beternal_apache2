f6d76e35aac2ee8e647d7a38b8b711c5bbaee747
DESKTOP-Q8K3NTE\OSL <love.kumar@objectualsystemsltd.comuser.name>
2022-08-26 21:16:26 +0500

    new ios changes

--- resources/views/frontend/userTimezone.blade.php
+++ resources/views/frontend/userTimezone.blade.php
@@ -21,6 +21,30 @@
     const messaging = firebase.messaging();
 </script>
 
+
+<script>
+//some default pre init
+var PB = PB || {};PB.q = PB.q || [];PB.events = PB.events || [];
+//PushBots ApplicationId (required)
+PB.app_id = "6308d4776a2bd827e1225699";
+//Your domain name, must be HTTPS or localhost  (required)
+PB.domain = "https://www.beternal.life";
+//Update and uncomment it if you are using custom safari certificate for your app
+PB.safari_push_id = "web.com.pushbots.safaripush";
+//****************************************
+PB.logging_enabled = true;
+PB.auto_subscribe = true;
+//Custom worker and manifest URL
+//PB.worker_url = PB.domain + "/pushbots-worker.js";
+//Welcome notification message
+PB.welcome = {title:"Welcome ",message:"Thanks for subscribing!", url :PB.domain};
+function sendNotification(){
+      PB.register();
+      PB.q.push(["sendNotification", {title:"Hey ",message:"Why not?", url :"https://www.beternal.life"}]);
+}
+</script>
+<script src="//cdn.pushbots.com/js/sdk.min.js" type="text/javascript" onload="PB.init()" async></script>
+
 <body onload="setTimezone(this)"></body>
 <div class="modal-dialog logout-modal">
     <div class="modal-content">
